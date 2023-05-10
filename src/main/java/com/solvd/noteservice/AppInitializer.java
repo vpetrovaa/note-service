e31d9d8949f8c507f555d983c6bac2b1b2c75027
vpetrovaa <vpetrova.laba@solvd.com>
2023-05-10 16:45:38 +0300

    Some changes were added

--- src/main/java/com/solvd/noteservice/AppInitializer.java
+++ src/main/java/com/solvd/noteservice/AppInitializer.java
@@ -2,10 +2,14 @@ package com.solvd.noteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ConfigurationPropertiesScan
+@EnableFeignClients
 public class AppInitializer {
 
 	public static void main(String[] args) {
