788e4b539353756e0dcbce51360a5c47da127479
vpetrovaa <vpetrova.laba@solvd.com>
2023-05-18 21:47:24 +0300

    Tests were fixed

--- src/test/java/com/solvd/noteservice/NoteServiceTest.java
+++ src/test/java/com/solvd/noteservice/NoteServiceTest.java
@@ -8,26 +8,18 @@ import com.solvd.noteservice.repository.NoteRepository;
 import com.solvd.noteservice.service.NoteService;
 import com.solvd.noteservice.service.client.ElasticClient;
 import com.solvd.noteservice.service.impl.NoteServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.doAnswer;
 
 @ExtendWith(MockitoExtension.class)
 public class NoteServiceTest {
@@ -55,37 +47,39 @@ public class NoteServiceTest {
     @Test
     void verifyExistsByIdPassedTest() {
         Long noteId = 1L;
-        when(elasticClient.isExistById(anyLong())).thenReturn(false);
+        Mockito.when(elasticClient.isExistById(Mockito.anyLong())).thenReturn(false);
         boolean result = noteService.isExistById(noteId);
-        assertEquals(false, result, "Assert that method returns false");
-        verify(elasticClient, times(1)).isExistById(noteId);
+        Assertions.assertEquals(false, result, "Assert that method returns false");
+        Mockito.verify(elasticClient, Mockito.times(1)).isExistById(noteId);
     }
 
     @Test
     void verifyCreatePassedTest() {
         Note note = NoteFactory.generateNote();
         Long noteId = 1L;
-        note.setId(null);
-        when(restTemplate.getForObject(anyString(), any(), anyLong())).thenReturn(true);
-        doAnswer(invocation -> {
-            Note noteCreated = invocation.getArgument(0);
-            noteCreated.setId(noteId);
-            return noteCreated;
-        }).when(noteRepository).save(note);
+        Mockito.when(restTemplate.getForObject(
+                Mockito.anyString(), Mockito.any(), Mockito.anyLong())
+        ).thenReturn(true);
+        Mockito.when(noteRepository.save(Mockito.any())).thenReturn(note);
         note = noteService.create(note);
-        assertEquals(noteId, note.getId(), "Assert that notes id are equals");
-        verify(restTemplate, times(1)).getForObject(anyString(), any(), anyLong());
-        verify(noteRepository, times(1)).save(note);
-        verify(kfProducer, times(1)).sendMessage(any());
+        Assertions.assertEquals(noteId, note.getId(), "Assert that notes id are equals");
+        Mockito.verify(restTemplate, Mockito.times(1))
+                .getForObject(Mockito.anyString(), Mockito.any(), Mockito.anyLong());
+        Mockito.verify(noteRepository, Mockito.times(1)).save(note);
+        Mockito.verify(kfProducer, Mockito.times(1))
+                .sendMessage(Mockito.any());
     }
 
     @Test
     void verifyCreateFailedTest() {
         Note note = NoteFactory.generateNote();
-        when(restTemplate.getForObject(anyString(), any(), anyLong())).thenReturn(false);
-        assertThrows(ResourceDoesNotExistException.class, ()-> noteService.create(note),
+        Mockito.when(restTemplate.getForObject(
+                Mockito.anyString(), Mockito.any(), Mockito.anyLong())
+        ).thenReturn(false);
+        Assertions.assertThrows(ResourceDoesNotExistException.class, ()-> noteService.create(note),
                 "Assert ResourceAlreadyExistsException");
-        verify(restTemplate, times(1)).getForObject(anyString(), any(), anyLong());
+        Mockito.verify(restTemplate, Mockito.times(1))
+                .getForObject(Mockito.anyString(), Mockito.any(), Mockito.anyLong());
     }
 
     @Test
@@ -93,20 +87,20 @@ public class NoteServiceTest {
         Note note = NoteFactory.generateNote();
         List<Note> notes = new ArrayList<>(List.of(note));
         Long userId = 1L;
-        when(elasticClient.findAllByUserId(anyLong())).thenReturn(notes);
+        Mockito.when(elasticClient.findAllByUserId(Mockito.anyLong())).thenReturn(notes);
         List<Note> notesFounded = noteService.findAllByUserId(userId);
-        assertEquals(notes, notesFounded, "Assert that notes and notesFounded are equals");
-        verify(elasticClient, times(1)).findAllByUserId(userId);
+        Assertions.assertEquals(notes, notesFounded, "Assert that notes and notesFounded are equals");
+        Mockito.verify(elasticClient, Mockito.times(1)).findAllByUserId(userId);
     }
 
     @Test
     void verifyFindAllTest() {
         Note note = NoteFactory.generateNote();
         List<Note> notes = new ArrayList<>(List.of(note));
-        when(elasticClient.findAll()).thenReturn(notes);
+        Mockito.when(elasticClient.findAll()).thenReturn(notes);
         List<Note> notesFounded = noteService.findAll();
-        assertEquals(notes, notesFounded, "Assert that notes and notesFounded are equals");
-        verify(elasticClient, times(1)).findAll();
+        Assertions.assertEquals(notes, notesFounded, "Assert that notes and notesFounded are equals");
+        Mockito.verify(elasticClient, Mockito.times(1)).findAll();
     }
 
 
@@ -114,19 +108,19 @@ public class NoteServiceTest {
     void verifyFindByIdPassedTest() {
         Note note = NoteFactory.generateNote();
         Long noteId = 1L;
-        when(elasticClient.findById(anyLong())).thenReturn(Optional.of(note));
+        Mockito.when(elasticClient.findById(Mockito.anyLong())).thenReturn(Optional.of(note));
         Note noteFounded = noteService.findById(noteId);
-        assertEquals(noteFounded, noteFounded, "Assert that note and noteFounded are equals");
-        verify(elasticClient, times(1)).findById(noteId);
+        Assertions.assertEquals(noteFounded, noteFounded, "Assert that note and noteFounded are equals");
+        Mockito.verify(elasticClient, Mockito.times(1)).findById(noteId);
     }
 
     @Test
     void verifyFindByIdFailedTest() {
         Long noteId = 1L;
-        when(elasticClient.findById(anyLong())).thenReturn(Optional.empty());
-        assertThrows(ResourceDoesNotExistException.class, () -> noteService.findById(noteId),
+        Mockito.when(elasticClient.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+        Assertions.assertThrows(ResourceDoesNotExistException.class, () -> noteService.findById(noteId),
                 "Assert ResourceDoesNotExistException");
-        verify(elasticClient, times(1)).findById(noteId);
+        Mockito.verify(elasticClient, Mockito.times(1)).findById(noteId);
     }
 
     @Test
@@ -135,13 +129,13 @@ public class NoteServiceTest {
         Long noteId = 1L;
         String newDescription = "new description";
         note.setDescription(newDescription);
-        when(noteRepository.findById(anyLong())).thenReturn(Optional.of(note));
-        when(noteRepository.save(any())).thenReturn(note);
+        Mockito.when(noteRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(note));
+        Mockito.when(noteRepository.save(Mockito.any())).thenReturn(note);
         Note noteUpdated = noteService.update(note);
-        assertEquals(note, noteUpdated, "Assert that notes are equals");
-        verify(noteRepository, times(1)).findById(noteId);
-        verify(noteRepository, times(1)).save(note);
-        verify(kfProducer, times(1)).sendMessage(any());
+        Assertions.assertEquals(note, noteUpdated, "Assert that notes are equals");
+        Mockito.verify(noteRepository, Mockito.times(1)).findById(noteId);
+        Mockito.verify(noteRepository, Mockito.times(1)).save(note);
+        Mockito.verify(kfProducer, Mockito.times(1)).sendMessage(Mockito.any());
     }
 
     @Test
@@ -151,18 +145,18 @@ public class NoteServiceTest {
         Note updatedNote = NoteFactory.generateNote();
         String themeForMismatch = "new description";
         updatedNote.setTheme(themeForMismatch);
-        when(noteRepository.findById(anyLong())).thenReturn(Optional.of(updatedNote));
-        assertThrows(IllegalOperationException.class, () -> noteService.update(note),
+        Mockito.when(noteRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(updatedNote));
+        Assertions.assertThrows(IllegalOperationException.class, () -> noteService.update(note),
                 "Assert ResourceDoesNotExistException");
-        verify(noteRepository, times(1)).findById(noteId);
+        Mockito.verify(noteRepository, Mockito.times(1)).findById(noteId);
     }
 
     @Test
     void verifyDeleteTest() {
         Long noteId = 1L;
         noteService.delete(noteId);
-        verify(noteRepository, times(1)).deleteById(noteId);
-        verify(kfProducer, times(1)).sendMessage(any());
+        Mockito.verify(noteRepository, Mockito.times(1)).deleteById(noteId);
+        Mockito.verify(kfProducer, Mockito.times(1)).sendMessage(Mockito.any());
     }
 
 }
