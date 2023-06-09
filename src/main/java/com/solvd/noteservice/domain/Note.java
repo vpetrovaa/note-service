ebe08ece436c318e289e280026baf0a6b7581b75
vpetrovaa <vpetrova.laba@solvd.com>
2023-06-09 14:09:27 +0300

    Integration tests were created

--- src/main/java/com/solvd/noteservice/domain/Note.java
+++ src/main/java/com/solvd/noteservice/domain/Note.java
@@ -8,11 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "notes")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Note {
 
     @Id
